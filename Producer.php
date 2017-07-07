8184646688feef0f93ed432f5ac0e5f0ee9f7b67
backfielder <backfielder@163.com>
2017-07-07 11:52:48 +0800

    Update Producer.php

--- Producer.php
+++ Producer.php
@@ -14,12 +14,17 @@ $config->setMetadataBrokerList('kafka0628:9092');
 $config->setBrokerVersion('0.10.1.0');                                                                                                                           
 $config->setRequiredAck(1);                                                                                                                                      
 $config->setIsAsyn(false);                                                                                                                                       
-$config->setProduceInterval(500);                                                                                                                                
+$config->setProduceInterval(500);
+
+echo "Will send a demo message to Kafka Service......";                                                                                                          
+echo date('Y-m-d H:i:s').',77.373,110.89,6.769,7.698,-0.52,-0.69,-0.48,0.0,-0.01,0.0,15.9,15.9,16.0,675.2,980.172,6557.186,-0.012,0.016,0.0,0.0,0.0,2100.0,29,38,
+503,284,86,1';
+
 $producer = new \Kafka\Producer(function() {                                                                                                                     
         return array(                                                                                                                                            
                 array(                                                                                                                                           
                         'topic' => 'test0706',                                                                                                                 
-                        'value' => '2016-05-01 00:00:05,77.373,10.89,6.769,7.698,-0.52,-0.69,-0.48,0.0,-0.01,0.0,15.9,15.9,16.0,675.2,980.172,6557.186,-0.012,0.016,0.0,0.0,0.0,2100.0,29,38,503,284,86,1',                                                                                                           
+                        'value' => date('Y-m-d H:i:s').',77.373,10.89,6.769,7.698,-0.52,-0.69,-0.48,0.0,-0.01,0.0,15.9,15.9,16.0,675.2,980.172,6557.186,-0.012,0.016,0.0,0.0,0.0,2100.0,29,38,503,284,86,1',                                                                                                           
                         'key' => '',  
                 ),                                                                                                                                               
         );                                                                                                                                                       
@@ -32,3 +37,5 @@ $producer->error(function($errorCode, $context) {
         var_dump($errorCode);                                                                                                                                    
 }); 
 $producer->send();
+
+echo 'Demo message send OK!';
